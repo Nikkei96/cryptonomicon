88778485eeecca1c7183d997590cab5d643efba6
Nikkei96 <nikitarudentsov@yandex.ru>
2021-03-09 11:48:51 +0300

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  printWidth: 150,
+}
